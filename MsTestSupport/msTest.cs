49467d6a2a2f9ccc186806b290cabfc40bae44fa
DmitryOlenin <molenin@mail.ru>
2018-03-16 19:18:39 +0300

    Nunit Add

---
+++ MsTestSupport/msTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+
+namespace Microsoft.VisualStudio.TestTools.UnitTesting
+{
+    public class Placeholder { }
+    public class TestClassAttribute : NUnit.Framework.TestFixtureAttribute
+    {
+    }
+    public class TestInitializeAttribute : NUnit.Framework.SetUpAttribute
+    {
+    }
+    public class TestMethodAttribute : NUnit.Framework.TestAttribute
+    {
+    }
+    public class TestCleanupAttribute : NUnit.Framework.TearDownAttribute
+    {
+    }
+    public class IgnoreAttribute : NUnit.Framework.IgnoreAttribute
+    {
+    }
+    public class ExpectedExceptionAttribute : NUnit.Framework.ExpectedExceptionAttribute
+    {
+        public ExpectedExceptionAttribute(Type exceptionType) : this(exceptionType, null)
+        {
+        }
+        public ExpectedExceptionAttribute(Type exceptionType, string message) : base(exceptionType)
+        {
+            UserMessage = message;
+        }
+    }
+    public class TestContext : NUnit.Framework.TestContext
+    {
+        public TestContext(IDictionary dictionary) : base(dictionary)
+        {
+        }
+    }
+    public class Assert : NUnit.Framework.Assert
+    {
+        public static void IsInstanceOfType(object obj, Type type)
+        {
+            NUnit.Framework.Assert.IsInstanceOfType(type, obj, null);
+        }
+        public static void IsInstanceOfType(object obj, Type type, string message)
+        {
+            NUnit.Framework.Assert.IsInstanceOfType(type, obj, message);
+        }
+    }
+    public class CollectionAssert : NUnit.Framework.CollectionAssert
+    {
+        public static new void AreEqual(IEnumerable obj, IEnumerable obj2, string message)
+        {
+            NUnit.Framework.CollectionAssert.AreEqual(obj, obj2, message);
+        }
+
+    }
+}
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ArrangeWF;
 using System.Collections.Generic;
-using System.Linq;
+using NUnit.Framework;
 
 namespace UnitTest
 {
-    [TestClass]
-    public class UnitTest1
+
+    [TestFixture]
+    public class NUnitDemo
     {
-        [TestMethod]
+        [Test]
         public void CitySortingTest()
         {
             // arrange
@@ -29,7 +29,33 @@ namespace UnitTest
 
             //Assert.AreEqual(cardsGood, cardsSorted, @"Неверный алгоритм сортировки");
             CollectionAssert.AreEqual(cardsGood, cardsSorted, @"Неверный алгоритм сортировки");
-
         }
     }
+
+    //[TestClass]
+    //public class UnitTest1
+    //{
+    //    [TestMethod]
+    //    public void CitySortingTest()
+    //    {
+    //        // arrange
+
+    //        var frm = new Form1();
+    //        var card1 = new Form1.Card { CityFrom = "Мельбурн", CityTo = "Кельн" };
+    //        var card2 = new Form1.Card { CityFrom = "Москва", CityTo = "Париж" };
+    //        var card3 = new Form1.Card { CityFrom = "Кельн", CityTo = "Москва" };
+    //        var cards = new List<Form1.Card> { card1, card2, card3 };
+    //        var cardsGood = new List<Form1.Card> { card1, card3, card2 };
+
+    //        // act
+
+    //        var cardsSorted = frm.arrange(cards);
+
+    //        // assert
+
+    //        //Assert.AreEqual(cardsGood, cardsSorted, @"Неверный алгоритм сортировки");
+    //        CollectionAssert.AreEqual(cardsGood, cardsSorted, @"Неверный алгоритм сортировки");
+
+    //    }
+    //}
 }
